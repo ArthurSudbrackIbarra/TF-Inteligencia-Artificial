02d186dd6921e54e45baf3a70dff3619bb38cda3
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-06-28 21:59:21 -0300

    Fixes após Sílvia.

--- generator.py
+++ generator.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from os import path
+from random import shuffle
 from sys import argv
 from re import sub
 
@@ -69,61 +70,85 @@ def main():
 
     # Palavras a serem desconsideradas.
     wordsToDesconsider = ["dell", "notebook",
-                          "not", "nov", "quer", "q", "ter", "ach", "agor", "vai", "dess", "inspiron", "precis", "cas", "%", "'s", "'tou"]
+                          "not", "nov", "quer", "q", "ter", "ach", "agor", "vai", "dess", "inspiron", "1", "precis", "cas", "%", "'s", "'tou"]
 
     print(f"\nPalavras sendo desconsideradas: {wordsToDesconsider}")
 
-    # Contando a aparição de cada palavra nas frases.
-    wordCountDict = {}
-    for data in dataList:
+    # TODO: SEPARAR CONJUNTO DE TREINO DO DE TESTE.
+
+    # Embaralhando os dados.
+    shuffle(dataList)
+
+    trainDataList = dataList[:int(len(dataList) * 0.7)]
+    testDataList = dataList[int(len(dataList) * 0.7):]
+
+    # Contando a aparição de cada palavra nas frases no conjunto de treino.
+    trainWordCountDict = {}
+    for data in trainDataList:
         for word in data.words:
             if word[1] not in wordsToDesconsider:
-                if word[1] not in wordCountDict:
-                    wordCountDict[word[1]] = 1
+                if word[1] not in trainWordCountDict:
+                    trainWordCountDict[word[1]] = 1
                 else:
-                    wordCountDict[word[1]] += 1
+                    trainWordCountDict[word[1]] += 1
 
     # Definindo quantas palavras serão utilizadas para o bag of words.
     if not userInformedK:
         k = int(input("\nQuantas palavras serão consideradas para o bag of words? "))
 
-    topWords = sorted(wordCountDict.items(),
+    if not userInformedFileName:
+        fileName = f"{k}-words.arff"
+
+    topWords = sorted(trainWordCountDict.items(),
                       key=lambda x: x[1], reverse=True)[0:k]
 
-    # Atribuindo 0 ou 1 a cada palavra para indicar se ela está ou não na frase.
-    matrixLines = []
-    for index, data in enumerate(dataList):
-        matrixLines.append([])
+    # Atribuindo 0 ou 1 a cada palavra para indicar se ela está ou não na frase (conjunto de treino).
+    trainMatrixLines = []
+    for index, data in enumerate(trainDataList):
+        trainMatrixLines.append([])
         for topWord in topWords:
             found = False
             for word in data.words:
                 if topWord[0] == word[1]:
-                    matrixLines[index].append(1)
+                    trainMatrixLines[index].append(1)
                     found = True
                     break
             if not found:
-                matrixLines[index].append(0)
-        matrixLines[index].append(data.dataClass)
+                trainMatrixLines[index].append(0)
+        trainMatrixLines[index].append(data.dataClass)
 
-    # Gerando o arquivo ARFF.
+    # Atribuindo 0 ou 1 a cada palavra para indicar se ela está ou não na frase (conjunto de teste).
+    testMatrixLines = []
+    for index, data in enumerate(testDataList):
+        testMatrixLines.append([])
+        for topWord in topWords:
+            found = False
+            for word in data.words:
+                if topWord[0] == word[1]:
+                    testMatrixLines[index].append(1)
+                    found = True
+                    break
+            if not found:
+                testMatrixLines[index].append(0)
+        testMatrixLines[index].append(data.dataClass)
+
+    # Gerando os arquivos ARFF.
     relation = "@relation emotionAnalysis\n"
     attributes = ""
     for topWord in topWords:
         attributes += f"@attribute {topWord[0]} {'{0,1}'}\n"
     attributes += "@attribute class {-1,1}\n"
-    inputData = "@data\n"
-    testInputData = inputData
-    for line in matrixLines:
+    trainInputData = "@data\n"
+    testInputData = "@data\n"
+    for line in trainMatrixLines:
         if 1 in line:
-            inputData += ",".join(map(str, line))
-            inputData += "\n"
+            trainInputData += ",".join(map(str, line))
+            trainInputData += "\n"
+    for line in testMatrixLines:
         testInputData += ",".join(map(str, line))
         testInputData += "\n"
-    trainFileContent = f"{relation}\n{attributes}\n{inputData}"
+    trainFileContent = f"{relation}\n{attributes}\n{trainInputData}"
     testFileContent = f"{relation}\n{attributes}\n{testInputData}"
-
-    if not userInformedFileName:
-        fileName = input("Digite o nome dos arquivos ARFF's a serem gerados: ")
 
     if not fileName.endswith(".arff"):
         fileName += ".arff"
@@ -138,9 +163,9 @@ def main():
 
     print("\nArquivos gerados com sucesso:\n")
     print(
-        f"train-datasets/{fileName}: Arquivo de treino, não considera linhas totalmente zeradas.")
+        f"train-datasets/{fileName}: Arquivo de treino.")
     print(
-        f"test-datasets/{fileName}: Arquivo de teste, considera todo o dataset.\n")
+        f"test-datasets/{fileName}: Arquivo de teste.\n")
 
 
 if __name__ == '__main__':
